39dc61645feeef2d3bbb8fe4251b941bcdb0a52d
gunm <gunm@cisco.com>
2018-05-01 14:05:38 +0530

    Barcode encode and decode changes done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions,BarcodeScanResult } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  result : BarcodeScanResult;
+  dataToEncode : String;
+  constructor(private barcode: BarcodeScanner) {}
+  async scanBarcode(){
+    try{
+      const options : BarcodeScannerOptions = {
+        torchOn :true,
+        prompt: 'Point your camera at barcode',
 
-  constructor(public navCtrl: NavController) {
+      }
+      await this.barcode.scan(options); 
+    }catch(error){
+      console.log(error)
+    }
 
+  }
+
+  async encodeData() {
+    try{
+      await this.barcode.encode(this.barcode.Encode.TEXT_TYPE, this.dataToEncode); 
+    }catch(error){
+      console.log(error)
+    }
   }
 
 }
