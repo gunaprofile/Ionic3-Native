4997845448a998a52ef90ad2054cb2286bd38f03
gunm <gunm@cisco.com>
2018-05-01 22:41:14 +0530

    Ionic 3 camera changes done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  image : String;
+  constructor(private camera: Camera) {
+    
   }
+  pictureFromCamera(){
+    const options: CameraOptions = {
+          quality: 100,
+          destinationType: this.camera.DestinationType.DATA_URL,
+          encodingType: this.camera.EncodingType.JPEG,
+          mediaType: this.camera.MediaType.PICTURE,
+          correctOrientation: true,
+          saveToPhotoAlbum: true
+    }
+    //Take a photo
+    this.takePhoto(options);
+  }
+  pictureFromGallery(){
+    const options: CameraOptions = {
+          quality: 100,
+          destinationType: this.camera.DestinationType.DATA_URL,
+          sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+          encodingType: this.camera.EncodingType.JPEG,
+          mediaType: this.camera.MediaType.PICTURE,
+          correctOrientation: true,
+          saveToPhotoAlbum: true
+    }
+    //Take a photo
+    this.takePhoto(options);
+  }
+
+  async takePhoto(options : CameraOptions){
+    try{
+      // take photo and store result
+      const result = await this.camera.getPicture(options);
+
+      //Append this to the dom
+      this.image = `data:image/jpeg;base64,${result}`
+    }catch(error){
+      console.log(error)
+    }
+  }
+  
 
 }
