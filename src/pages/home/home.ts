8636b1c1bb1444537a83a3e6b6ba703058a9b943
gunm <gunm@cisco.com>
2018-05-01 09:30:41 +0530

    AdMod Free changes done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig} from '@ionic-native/admob-free';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public admobfree: AdMobFree) {
+    this.showAdBanner();
+    this.showInitialAd();
+  }
 
+  async showAdBanner(){
+    const bannerConfig : AdMobFreeBannerConfig ={
+      //id : 'ca-pub-XXXXXXXXXX',
+      isTesting : true,
+      autoShow : true,
+      // bannerAtTop : true,
+
+    }
+    this.admobfree.banner.config(bannerConfig);
+    try{
+      const result = this.admobfree.banner.prepare();
+      console.log(result)
+    }catch(error){
+      console.log(error);
+    }
+
+  }
+  async showInitialAd(){
+    try{
+      const interstitialConfig: AdMobFreeInterstitialConfig = {
+        isTesting : true,
+        autoShow : true,
+      }
+      this.admobfree.interstitial.config(interstitialConfig);
+      const result = await this.admobfree.interstitial.prepare();
+      console.log(result);
+    }catch(error){
+      console.log(error);
+    }
   }
 
 }
