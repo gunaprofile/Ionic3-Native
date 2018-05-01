a564ac733951ccc9d030f70aafc86af102daa7e9
gunm <gunm@cisco.com>
2018-05-01 08:12:52 +0530

    Native Action sheet changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//Import Actionsheet
+import { ActionSheet } from '@ionic-native/action-sheet';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -24,7 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ActionSheet
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
+//Import Actionsheet
+import { ActionSheet, ActionSheetOptions } from '@ionic-native/action-sheet';
+let buttonLabelsToShow = ['Share via Facebook', 'Share via Twitter'];
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+   /** Action Sheet Options */
+  options: ActionSheetOptions = {
+    title: 'Share This Image with the World',
+    subtitle: 'Choose from the various Options',
+    buttonLabels: buttonLabelsToShow,
+    addCancelButtonWithLabel: 'Cancel',
+    addDestructiveButtonWithLabel: 'Delete',
+    destructiveButtonLast: true
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(private actionSheet: ActionSheet) {}
 
+  showActionSheet() {
+    this.actionSheet.show(this.options).then((buttonClickedIndex: number) => {
+      console.log('Button pressed is' + buttonClickedIndex)
+    }).catch((err) => {
+      console.log(err)
+    });
   }
 
 }
