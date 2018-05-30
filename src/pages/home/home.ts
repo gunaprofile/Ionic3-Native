332add01680d6b91025f921936195c073bcf26f2
gunm <gunm@cisco.com>
2018-05-30 11:37:17 +0530

    Flashlight

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  isOn: boolean;
+  constructor(private flashlight: Flashlight, private platform : Platform) {
+    this.platform.ready().then(()=>{
+      this.updateFlashLightStatus();
+    });
   }
-
+  switchOn(){
+    this.flashlight.switchOn();
+    this.updateFlashLightStatus()
+  }
+  switchOff(){
+    this.flashlight.switchOff();
+    this.updateFlashLightStatus()
+  }
+  toggle(){
+    this.flashlight.toggle();
+    this.updateFlashLightStatus()
+  }
+  updateFlashLightStatus(){
+    this.isOn = this.flashlight.isSwitchedOn();
+  }
 }
